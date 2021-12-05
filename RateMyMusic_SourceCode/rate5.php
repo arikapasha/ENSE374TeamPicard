5947ee01108cb81334127a85cd007c17171bc962
arikapasha <90287606+arikapasha@users.noreply.github.com>
2021-12-05 11:40:20 -0600

    Add files via upload

---
+++ RateMyMusic_SourceCode/rate5.php
@@ -0,0 +1,36 @@
+<?php
+    ini_set('display_errors', 1);
+        session_start();
+        if (!isset($_SESSION["username"])) {
+            header("Location: rmmlogin.html");
+            exit();
+        }
+        else {
+     //connection to db
+     include 'config.php';
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $songid = $_GET['sid'];
+    $name = $_GET['sname'];
+    $singer = $_GET['aname'];
+    $img = $_GET['im'];
+    $user = $_SESSION["username"];
+    $r = '5';
+
+
+
+            $sql = " INSERT INTO rate (user, song1, artist1, pic1, sid1, rate ) VALUES ( '$user', '$name', '$singer', '$img', '$songid', '$r' ) ";
+    if ($conn->query($sql) === TRUE) {
+      header('Location: userpage.php');
+      
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+}
+  ?>
